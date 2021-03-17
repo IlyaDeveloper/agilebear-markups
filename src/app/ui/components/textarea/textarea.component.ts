f1385d496e98cc4004d75d2d5e4e04eeed2f2b2c
Ilya Pashchuk <twic@justtwic.local>
2021-03-18 01:24:19 +0200

    Stage: update textarea

--- src/app/ui/components/textarea/textarea.component.ts
+++ src/app/ui/components/textarea/textarea.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'agl-textarea',
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextareaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('inputRef') inputRef: ElementRef;
 
-  ngOnInit(): void {
+  @Input() label: string;
+  @Input() placeholder = 'Enter text';
+  @Input() submitted: boolean;
+  @Input() value: any;
+  @Input() type = 'text';
+  @Input() inputMode = 'text';
+  @Input() autofocus: boolean;
+  @Input() errorsMsg: string;
+
+  isError: any = false;
+  disabled: any = false;
+
+
+  constructor() {
   }
 
+  ngOnInit(): void {
+    if (this.autofocus) {
+      window.setTimeout(() => {
+        this.inputRef.nativeElement.focus();
+      }, 100);
+    }
+  }
+
+  public onChange(event) {
+  }
+
+  public onFocus(event) {
+
+    setTimeout(() => {
+      this.inputRef.nativeElement.selectionStart = 1;
+      this.inputRef.nativeElement.selectionEnd = 1;
+      this.inputRef.nativeElement.focus();
+    });
+  }
+
+  public onBlur(event) {
+  }
 }
