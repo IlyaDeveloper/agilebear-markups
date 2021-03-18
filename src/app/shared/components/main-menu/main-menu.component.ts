ef2233a68a8c92ad2d6d3306a878038a8c672c29
Ilya Pashchuk <twic@justtwic.local>
2021-03-18 16:17:04 +0200

    Fixed: reaponsive

--- src/app/shared/components/main-menu/main-menu.component.ts
+++ src/app/shared/components/main-menu/main-menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, Output, EventEmitter, OnInit} from '@angular/core';
+import {Component, Output, EventEmitter, OnInit, Input} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -10,16 +10,33 @@ export class MainMenuComponent implements OnInit {
 
   @Output() clickLinkEvent: EventEmitter<any> = new EventEmitter();
 
+  @Input() sunMenu1: any = [];
+  @Input() sunMenu2: any = [];
+
   constructor(
     private router: Router,
   ) {
   }
+
+
+  sub1: boolean = false;
+  sub2: boolean = false;
 
   ngOnInit(): void {
   }
 
   closeMenu() {
     this.clickLinkEvent.emit();
+    this.sub1 = false;
+    this.sub2 = false;
+  }
+
+  openSub(val?: string) {
+    if (val === 'services') {
+      this.sub1 = !this.sub1;
+    } else if (val === 'training') {
+      this.sub2 = !this.sub2;
+    }
   }
 
   goToHome() {
